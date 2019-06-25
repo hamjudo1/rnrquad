2580144316faf6a2a3e7a1db9ced0e7b41081ed4
Florian <florian@notion.ai>
2019-06-25 15:18:35 -0400

    added nice PID object

--- Libraries/rnrquad/src/objects/src/PID.cpp
+++ Libraries/rnrquad/src/objects/src/PID.cpp
@@ -1,26 +1,36 @@
-//#include "PID.h"
-//
-//
-//void PID::tstep(float value)
-//{
-//  proportional = (target - value) / target;
-//  derivative = value - (previous_value ? : value) / target;
-//  integral = value - (previous_value ? : value) / target;
-//
-//}
-//
-///**
-// * Initializes a PID object with given constants
-// *
-// * @param target_value
-// * @param proportional_constant Proportional Constant
-// * @param integral_constant Integral Constant
-// * @param derivative_constant Derivative Constant
-// */
-//void PID::begin(float target_value, float proportional_constant, float integral_constant, float derivative_constant)
-//{
-//  target = target_value;
-//  KP = proportional_constant;
-//  KI = integral_constant;
-//  KD = derivative_constant;
-//}
+#include "PID.h"
+
+/**
+ * Loop iteration
+ *
+ * @param value
+ * @return float
+ */
+float PID::tstep(float value)
+{
+  float error = target - value;
+  float proportional = error;
+  float derivative = value - (previous_value ? : value) / target;
+  float integral =  (.8 * accumulated_error + .2 * error);
+
+  previous_value = value;
+  accumulated_error = integral;
+
+  return (KP * proportional) + (KD * derivative) + (KI * integral);
+}
+
+/**
+ * Initializes a PID object with given constants
+ *
+ * @param target_value
+ * @param proportional_constant Proportional Constant
+ * @param integral_constant Integral Constant
+ * @param derivative_constant Derivative Constant
+ */
+void PID::begin(float target_value, float proportional_constant, float integral_constant, float derivative_constant)
+{
+  target = target_value;
+  KP = proportional_constant;
+  KI = integral_constant;
+  KD = derivative_constant;
+}
--- Libraries/rnrquad/src/objects/src/PID.h
+++ Libraries/rnrquad/src/objects/src/PID.h
@@ -18,22 +18,16 @@ public:
   void setLogging(bool On);
 };
 
-//class PID
-//{
-//public:
-//  float PIDintegral;
-//  float PIDderivative;
-//  float prevError;
-//  float *PIDoutput;
-//  float *PIDerror;
-//  float *KD;
-//  float *KI;
-//  float *KP;
-//  char *prefix;
-//
-//  void begin(char *prefix, float *pError, float *pOutput, float *pKP, float *pKI, float *pKD);
-//
-//  void tstep(float dt);
-//
-//  void setLogging(bool On);
-//};
+class PID
+{
+public:
+  float target;
+  float KP;
+  float KI;
+  float KD;
+  float previous_value;
+  float accumulated_error;
+
+  void begin(float target_value, float proportional_constant, float integral_constant, float derivative_constant);
+  float tstep(float value);
+};
